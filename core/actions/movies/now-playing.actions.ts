1bb6a365bb05405169a3fe076626a62c1101f641
leonardo <leonardo.a@cliker.mx>
2024-12-17 14:14:50 -0600

    Mapear respuestas a objetos personalizados

--- core/actions/movies/now-playing.actions.ts
+++ core/actions/movies/now-playing.actions.ts
@@ -1,12 +1,17 @@
 import { movieApi } from "@/core/api/movie-api";
 import { MovieDBMoviesResponse } from "@/infrastructure/interfaces/moviedb-response";
+import { MovieMapper } from "@/infrastructure/mappers/movie.mapper";
 
 export const nowPlayingAction = async() => {
     try {
         const {data} = await movieApi.get<MovieDBMoviesResponse>('/now_playing')
-        console.log(JSON.stringify(data, null, 2));
+        //console.log(JSON.stringify(data, null, 2));
+
+        const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie)
+        //const movies = data.results.map((movie) => MovieMapper.fromTheMovieDBToMovie(movie)) // Se puede usar tambien de esta manera
         
-        return[];
+        console.log(movies)
+        return movies
 
     } catch (error) {
         console.log(error);
