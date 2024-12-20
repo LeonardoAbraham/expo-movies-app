76516f028f14865b661f769e1e2cf09868a0b119
leonardo <leonardo.a@cliker.mx>
2024-12-20 10:21:47 -0600

    Determinar fin de scroll de un FlatList

--- presentation/components/movies/MovieHorizontalList.tsx
+++ presentation/components/movies/MovieHorizontalList.tsx
@@ -1,14 +1,38 @@
 import { Movie } from '@/infrastructure/interfaces/movie.interface'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, NativeScrollEvent, NativeSyntheticEvent } from 'react-native'
 import MoviePoster from './MoviePoster';
+import { useRef } from 'react';
 
 interface Props {
     title?: string;
     movies: Movie[];
     className?: string;
+
+    loadNextPage?: () => void;
 }
 
-const MovieHorizontalList = ({title, movies, className}: Props) => {
+const MovieHorizontalList = ({title, movies, className, loadNextPage}: Props) => {
+
+    const isLoading = useRef(false);
+
+    const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+        if(isLoading.current) return;
+
+        const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
+
+        const isEndReached = (contentOffset.x + layoutMeasurement.width + 600 ) >= contentSize.width;
+
+        if(!isEndReached) return;
+
+        isLoading.current = true;
+
+        // TODO:
+        console.log('Cargar siguientes películas');
+
+        loadNextPage && loadNextPage();
+
+    }
+
     return (
         <View className={`${className}`}>
             { title &&  <Text className='text-3xl font-bold px-4 mb-2'>Populares</Text> }
@@ -21,6 +45,7 @@ const MovieHorizontalList = ({title, movies, className}: Props) => {
                 renderItem={({item}) => (
                     <MoviePoster id={ item.id } poster={ item.poster } smallPoster/>
                 )}
+                onScroll={onScroll}
             />
 
         </View>
